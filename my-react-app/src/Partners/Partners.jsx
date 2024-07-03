195f02daf862ca2601a4e8a5d9f7742bc3bdadf1
SarunasMif <gikys.sarunas@gmail.com>
2024-07-03 16:02:27 +0300

    partner cards made into props

--- my-react-app/src/Partners/Partners.jsx
+++ my-react-app/src/Partners/Partners.jsx
@@ -4,14 +4,16 @@ import People from '../People/People.jsx'
 function Partners() {
     return(
         <>
-        <div className={styles.H2}>
-            <h2>Our top partners</h2>
-        </div>
-        <div className={styles.people_container}>
-            <People></People>
-            <People></People>
-            <People></People>
-            <People></People>
+        <div className={styles.BODY}>
+            <div className={styles.H2}>
+                <h2>Our top partners</h2>
+            </div>
+            <div className={styles.people_container}>
+                <People name="Paulius Puskunigis" earnings={6000} description="Defrauded the national bank of Paraguy." status="KIA"></People>
+                <People></People>
+                <People></People>
+                <People></People>
+            </div>
         </div>
         </>
     );    
--- my-react-app/src/People/People.jsx
+++ my-react-app/src/People/People.jsx
@@ -1,14 +1,30 @@
 import styles from './People.module.css'
+import PropTypes from 'prop-types'
 
-function People(){
+function People(props){
 
     return(
         <div className={styles.Card}>
             <img src="https://via.placeholder.com/150"></img>
-            <h2 className={styles.Card_Title}>Paulius Puskunigis</h2>
-            <p className={styles.Card_Text}>Defrauded the national bank of paraguy</p>
+            <h2 className={styles.Card_Title}>{props.name}</h2>
+            <p className={styles.Card_Text}>Earnings: {props.earnings},00$</p>
+            <p className={styles.Card_Text}>{props.description}</p>
+            <p className={styles.Card_Text}>Status: {props.status}</p>
         </div>
     );
 }
 
+People.propTypes = {
+    name: PropTypes.string,
+    earnings: PropTypes.number,
+    description: PropTypes.string,
+    status: PropTypes.string,
+}
+
+People.defaultProps = {
+    name: "John Doe",
+    earnings: 0,
+    description: "Executed 10 bolivian children.",
+    status: "Active",
+}
 export default People
